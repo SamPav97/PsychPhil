2b39c69b05d9ff453fb75753c0be70293e2ab627
Samuil <samuil.pavlevchev@gmail.com>
2022-11-10 11:01:35 +0000

    Setting up views and temps

---
+++ PsychPhil_app/PsychPhil_app/therapies/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TherapiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PsychPhil_app.therapies'
---
+++ PsychPhil_app/PsychPhil_app/therapies/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+#do model for therapies then set up a db
